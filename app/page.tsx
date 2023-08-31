1f4bb29e3cd8169fdd108683d486b2ce07defe62
Zoe Thomas <zoethomas2893@gmail.com>
2023-08-31 16:30:41 +0100

    add click card and rsvp option

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import React, { useState } from "react";
+
+import ReactCardFlip from "react-card-flip";
+
 import styles from "./page.module.css";
 import { Press_Start_2P } from "next/font/google";
 
@@ -7,6 +13,7 @@ const roboto = Press_Start_2P({
 });
 
 export default function Home() {
+  const [rotated, setRotated] = useState(false);
   return (
     <div className="body">
       <main className={styles.main}>
@@ -16,21 +23,40 @@ export default function Home() {
             <p>ZOE TURNS 30!!</p>
           </div>
           <div className={styles.wordart}>You&apos;re Invited</div>
-          <div className={styles.card}>
-            <div className={styles["card-inner"]}>
-              <div className={styles["card-front"]}>
-                <p>
-                  28th October 2023 <br />
-                  6pm til late
-                </p>
+          <div className={roboto.className}>
+            <p>
+              click below for the
+              <br /><br />
+              {rotated ? `date` : `venue`}
+            </p>
+          </div>
+          <div onClick={() => setRotated((rotated) => !rotated)}>
+            <ReactCardFlip isFlipped={rotated} flipDirection="vertical">
+              <div className={styles.card}>
+                <div className={styles["card-inner"]}>
+                  <div className={styles["card-front"]}>
+                    <p>
+                      28th October 2023 <br />
+                      6pm til late
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className={styles["card-back"]}>
-                <p>
-                  Big Moose Coffee <br />
-                  Cardiff
-                </p>
+              <div className={styles.card}>
+                <div className={styles["card-inner"]}>
+                  <div className={styles["card-back"]}>
+                    <p>
+                      Big Moose Coffee <br />
+                      Cardiff
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
+            </ReactCardFlip>
+          </div>
+          <div className={roboto.className}>
+            <p>RSVP <a href="mailto:riyals-nickels-0e@icloud.com?subject=Your birthday party!">here</a>
+            </p>
           </div>
         </div>
       </main>
